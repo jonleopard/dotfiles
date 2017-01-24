fc58a5411e55b5adc51d568b7777da7cf3bf31ef
Jon Leopard <jontron2000@gmail.com>
2017-01-24 16:13:16 +0100

    Added pyenv to environment

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -1,10 +1,18 @@
-# Environment Variables
+# Environment Paths
 
-## Paths
+# Ruby (rbenv)
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
+
+# Python (pyenv)
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+
 
 ## Editors
 export EDITOR=nvim
