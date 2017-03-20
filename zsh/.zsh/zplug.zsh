17ee60d1d7427c0d33e7c3592cbb559eeb4128d2
Jon Leopard <jontron2000@gmail.com>
2017-03-20 13:29:10 +0100

    Moved zplug variables into zsh/zplug.zsh. Cleaned up .zshrc

---
+++ zsh/.zsh/zplug.zsh
@@ -0,0 +1,69 @@
+
+# load zplug
+export ZPLUG_HOME=/usr/local/opt/zplug
+source $ZPLUG_HOME/init.zsh
+
+zplug "so-fancy/diff-so-fancy", \
+      if:"(($+commands[git] + $+commands[perl] + $(perl -e 'use open qw(:std :utf8);' 2> /dev/null && echo 1 || echo 0) == 3))", \
+      as:command, \
+      use:"diff-so-fancy"
+
+zplug "nvie/gitflow", \
+      if:"(($+commands[git]))", \
+      as:command
+
+zplug "dracula/zsh", \
+      as:theme
+      ZSH_THEME="dracula"
+
+zplug "sindresorhus/pure", use:pure.zsh, defer:2
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+zplug "felixr/docker-zsh-completion"
+zplug "bobthecow/git-flow-completion"
+zplug "zsh-users/zsh-completions"
+zplug "zsh-users/zsh-history-substring-search", defer:3
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug "mafredri/zsh-async", on:sindresorhus/pure
+zplug "lukechilds/zsh-nvm"
+
+# FZF
+# Grab binaries from GitHub Releases
+# and rename with the "rename-to:" tag
+zplug "junegunn/fzf-bin", \
+    from:gh-r, \
+    as:command, \
+    rename-to:fzf, \
+    use:"*darwin*amd64*"
+
+
+fzf-down() {
+  fzf --height 50% "$@"
+}
+
+export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
+[ -n "$NVIM_LISTEN_ADDRESS" ] && export FZF_DEFAULT_OPTS='--no-height'
+    
+
+zplug 'knu/z', \
+    use:'z.sh', \
+    defer:1
+
+
+# Install packages that have not been installed yet
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    else
+        echo
+    fi
+fi
+
+[ -f ~/.zshrc.pluginconf ] && source ~/.zshrc.pluginconf
+
+# For FZF
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# Then, source plugins and add commands to $PATH
+zplug load
+
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,34 +1,5 @@
 # Load ZSH Config Files
 for config (~/.zsh/*.zsh) source $config
 
-# Load ZPLUG
-#export ZPLUG_HOME=/usr/local/opt/zplug
-#source $ZPLUG_HOME/init.zsh
-
 # No GAnalytics Logging in homebrew
 HOMEBREW_NO_ANALYTICS=1
-
-export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
-[ -n "$NVIM_LISTEN_ADDRESS" ] && export FZF_DEFAULT_OPTS='--no-height'
-    
-
-[ -f ~/.zshrc.pluginconf ] && source ~/.zshrc.pluginconf
-
-# For FZF
-#[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-if [[ -f ~/.zplug/init.zsh ]]; then
-    export ZPLUG_LOADFILE=~/.zsh/zplug.zsh
-    # For development version of zplug
-    # source ~/.zplug/init.zsh
-    source ~/src/github.com/zplug/zplug/init.zsh
-
-    if ! zplug check --verbose; then
-        printf "Install? [y/N]: "
-        if read -q; then
-            echo; zplug install
-        fi
-        echo
-    fi
-    zplug load
-fi
