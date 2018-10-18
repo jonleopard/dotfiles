b0cf851c881501bcb085113e2ad149117de36d2d
Jon Leopard <contact@jonleopard.com>
2018-10-18 10:18:15 +0200

    Re-add diff-so-fancy

--- zsh/.zsh/antibody.zsh
+++ zsh/.zsh/antibody.zsh
@@ -1,8 +1,8 @@
 # Uncomment 2-3 when adding new plugins.
-#source <(antibody init)
-#antibody bundle < ~/.zsh/bundles.txt
+source <(antibody init)
+antibody bundle < ~/.zsh/bundles.txt
 
 
 #After installing new plugins, comment lines 1-2 and uncomment 7-8
-antibody bundle < ~/.zsh/bundles.txt > ~/.zsh/bundles.sh
-source ~/.zsh/bundles.sh
+#antibody bundle < ~/.zsh/bundles.txt > ~/.zsh/bundles.sh
+#source ~/.zsh/bundles.sh
