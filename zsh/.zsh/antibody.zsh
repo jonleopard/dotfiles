655e18352c9799e34d5dd63e799bc95d52078761
Jon Leopard <jontron2000@gmail.com>
2019-01-09 11:44:52 +0100

    Adding zsh-nvm plugin. Also trying out antibody's static loading feature

--- zsh/.zsh/antibody.zsh
+++ zsh/.zsh/antibody.zsh
@@ -1,8 +1,11 @@
 # Uncomment 2-3 when adding new plugins.
-source <(antibody init)
-antibody bundle < ~/.zsh/bundles.txt
+#source <(antibody init)
+#antibody bundle < ~/.zsh/bundles.txt
 
 
-#After installing new plugins, comment lines 1-2 and uncomment 7-8
+# After installing new plugins, comment lines 1-2 and uncomment 7-8
+source ~/.zsh/bundles.sh
 #antibody bundle < ~/.zsh/bundles.txt > ~/.zsh/bundles.sh
-#source ~/.zsh/bundles.sh
+
+# Run line 8 in your console, this populates bundles.sh file with paths from
+# our bundles.txt file
--- zsh/.zsh/bundles.sh
+++ zsh/.zsh/bundles.sh
@@ -1,12 +1,11 @@
+source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm/zsh-nvm.plugin.zsh
+fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm )
 export PATH="/Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-ports:$PATH"
+export PATH="/Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-so-fancy-SLASH-diff-so-fancy:$PATH"
 source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
 fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
 source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
 fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
-source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
 source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair/autopair.plugin.zsh
 source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair/zsh-autopair.plugin.zsh
 fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-hlissner-SLASH-zsh-autopair )
@@ -14,6 +13,10 @@ fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com
 source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-felixr-SLASH-docker-zsh-completion/docker-zsh-completion.plugin.zsh
 fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-felixr-SLASH-docker-zsh-completion )
 
+source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
 source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
 fpath+=( /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
 source /Users/jon/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
