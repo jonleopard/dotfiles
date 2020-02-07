9d8b7971b4d2906a2c65590a0387f6a2fd561eea
Jon Leopard <jontron2000@gmail.com>
2020-02-07 15:28:33 +0100

    N now installed through homebrew.
    Keeping here for when I create linux installation.

--- zsh/.zsh/environment.zsh
+++ zsh/.zsh/environment.zsh
@@ -3,7 +3,7 @@
 
 # Node (n)
 # Note that this needs to be before homebrew
-export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
+# export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
 
 # homebrew
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
