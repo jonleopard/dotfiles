e72b4b16c38eb1e09a73415eb8f2a5ade2e0c088
Jon Leopard <jontron2000@gmail.com>
2021-02-15 17:29:23 +0100

    Removing yabai config

--- zsh/.zsh/fzf.zsh
+++ zsh/.zsh/fzf.zsh
@@ -26,16 +26,24 @@ export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap 
 
 if command -v fd > /dev/null; then
   export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
-  export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'
-  export FZF_CTRL_T_COMMAND='fd --type f --type d --hidden --follow --exclude .git'
+  export FZF_CTRL_T_COMMAND='fd --type f --hidden --follow --exclude .git'
 fi
 
-# This is for the fzf plugin
-export FZF_COMPLETION_TRIGGER=','
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
 
 
 command -v bat  > /dev/null && export FZF_CTRL_T_OPTS="--preview 'bat -n --color=always {}'"
-command -v tree > /dev/null && export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 
 
 
