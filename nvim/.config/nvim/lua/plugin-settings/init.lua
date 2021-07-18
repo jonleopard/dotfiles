bc539db4390e4441c7794b1750f8fcfe17670b8a
Jon Leopard <jontron2000@gmail.com>
2021-07-18 13:28:59 +0200

    Swapped out fzf for telescope

---
+++ nvim/.config/nvim/lua/plugin-settings/init.lua
@@ -0,0 +1,15 @@
+require("plugin-settings.telescope")
+
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
+if pcall(require, 'plenary') then
+  RELOAD = require('plenary.reload').reload_module
+
+  R = function(name)
+    RELOAD(name)
+    return require(name)
+  end
+end
---
+++ nvim/.config/nvim/lua/plugin-settings/telescope.lua
@@ -0,0 +1,48 @@
+local actions = require('telescope.actions')
+
+require('telescope').setup {
+    defaults = {
+        file_sorter = require('telescope.sorters').get_fzy_sorter,
+        prompt_prefix = ' >',
+        color_devicons = true,
+
+        file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
+        grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
+        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
+
+        mappings = {
+            i = {
+                ["<C-x>"] = false,
+                ["<C-q>"] = actions.send_to_qflist,
+            },
+        }
+    },
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true,
+        }
+    },
+}
+
+local M = {}
+
+M.search_dotfiles = function()
+    require("telescope.builtin").find_files({
+        prompt_title = "< VimRC >",
+        cwd = '$HOME/dotfiles/nvim/.config/nvim/',
+        hidden = true,
+    })
+end
+
+M.git_branches = function()
+    require("telescope.builtin").git_branches({
+        attach_mappings = function(_, map)
+            map('i', '<c-d>', actions.git_delete_branch)
+            map('n', '<c-d>', actions.git_delete_branch)
+            return true
+        end
+    })
+end
+
+return M
