26f6035173e8faea24f32feb5da9f71ceec94f41
Jon Leopard <jontron2000@gmail.com>
2021-07-18 21:28:11 +0200

    Don't need this

--- nvim/.config/nvim/lua/plugin-settings/compe.lua
+++ nvim/.config/nvim/lua/plugin-settings/compe.lua
@@ -33,9 +33,6 @@ require'compe'.setup {
 }
 
 
-
-
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 capabilities.textDocument.completion.completionItem.resolveSupport = {
@@ -67,33 +64,3 @@ vim.api.nvim_set_keymap('s', '<S-Tab>', 'v:lua.s_tab_complete()', { expr = true 
 vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")', { expr = true })
 vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', { expr = true })
 
-local eslint = {
-    lintCommand = 'eslint_d -f unix --stdin --stdin-filename ${INPUT}',
-    lintIgnoreExitCode = true,
-    lintStdin = true,
-    lintFormats = { '%f:%l:%c: %m' },
-    formatCommand = 'eslint_d --fix-to-stdout --stdin --stdin-filename=${INPUT}',
-    formatStdin = true,
-}
-
-local prettier = {
-    formatCommand = 'prettier --stdin-filepath ${INPUT}',
-    formatStdin = true,
-}
-
--- local luaformatter = { formatCommand = "lua-format -i", formatStdin = true }
-local stylua = { formatCommand = 'stylua -s -', formatStdin = true }
-
-return {
-    css = { prettier },
-    html = { prettier },
-    javascript = { prettier, eslint },
-    javascriptreact = { prettier, eslint },
-    json = { prettier },
-    lua = { stylua },
-    markdown = { prettier },
-    scss = { prettier },
-    typescript = { prettier, eslint },
-    typescriptreact = { prettier, eslint },
-    yaml = { prettier },
-}
