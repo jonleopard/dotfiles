ec4f966da1ed56c93ec0c86e2bf4ea870cd483aa
Jon Leopard <jontron2000@gmail.com>
2021-07-26 17:07:31 +0200

    Go config is working now

---
+++ nvim/.config/nvim/lua/plugin-settings/go.lua
@@ -0,0 +1,25 @@
+-- setup go.nvim
+require('go').setup({
+  goimport = 'gopls', -- if set to 'gopls' will use golsp format
+  gofmt = 'gopls', -- if set to gopls will use golsp format
+  max_line_line = 120,
+  tag_transform = false,
+  test_dir = '',
+  comment_placeholder = '   ',
+  lsp_cfg = true, -- false: use your own lspconfig
+  lsp_gofumpt = true, -- true: set default gofmt in gopls format to gofumpt
+  lsp_on_attach = true, -- use on_attach from go.nvim
+  dap_debug = true,
+})
+
+-- Lua doesn't have autocmd support yet
+-- Format on save
+vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').gofmt() ]], false)
+
+-- Import on save
+vim.api.nvim_exec([[ autocmd BufWritePre *.go :silent! lua require('go.format').goimport() ]], false)
+
+-- vim.cmd([[command! Gofmt lua require("go.format").goimport()]])
+-- vim.cmd([[command! Goimport lua require("go.format").goimport()]])
+
+local protocol = require'vim.lsp.protocol'
