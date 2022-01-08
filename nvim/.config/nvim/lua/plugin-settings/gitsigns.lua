7283f567c49e6d501a82fa75b94a4e47ff210fb6
Jon Leopard <contact@jonleopard.com>
2022-01-08 13:37:29 -0700

    Show git-blame messages

--- nvim/.config/nvim/lua/plugin-settings/gitsigns.lua
+++ nvim/.config/nvim/lua/plugin-settings/gitsigns.lua
@@ -6,4 +6,11 @@ require('gitsigns').setup {
     topdelete = { hl = 'GitGutterDelete', text = '‾' },
     changedelete = { hl = 'GitGutterChange', text = '~' },
   },
+  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 500,
+    ignore_whitespace = false,
+  },
 }
