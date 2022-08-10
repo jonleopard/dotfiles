1dc42680291ff02da3ef30524592894b5c3c8db7
Jon Leopard <contact@jonleopard.com>
2022-08-10 11:08:25 -0700

    feat: implement cht.sh

---
+++ bash/cht.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+selected=`cat ~/dotfiles/bash/.tmux-cht-languages ~/dotfiles/bash/.tmux-cht-command | fzf`
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/dotfiles/bash/.tmux-cht-languages; then
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
