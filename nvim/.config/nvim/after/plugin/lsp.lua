bde6b00a201659de93326ac366fa9384deceb84f
Jon Leopard <contact@jonleopard.com>
2022-11-04 16:00:33 -0700

    fix: update -> default api change

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -67,7 +67,7 @@ tabnine:setup({
 
 local function config(_config)
     return vim.tbl_deep_extend("force", {
-        capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+        capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
         on_attach = function()
             nnoremap("gd", function()
                 vim.lsp.buf.definition()
