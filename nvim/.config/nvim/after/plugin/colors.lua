c61a4359f4c47313f8573b0fa039b16eb4da6b8a
Jon Leopard <contact@jonleopard.com>
2022-12-19 16:01:04 -0700

    fix: org name change

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,6 +1,6 @@
 local fn = vim.fn
 local cmd = vim.cmd
-local set_theme_path = "$HOME/.config/base16-project/set_theme.lua"
+local set_theme_path = "$HOME/.config/tinted-theming/set_theme.lua"
 local is_set_theme_file_readable = fn.filereadable(fn.expand(set_theme_path)) == 1 and true or false
 
 if is_set_theme_file_readable then
