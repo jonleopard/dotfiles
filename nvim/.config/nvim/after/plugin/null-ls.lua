a3e9029489834bb6564aaa14b597b53199b05a1e
Jon Leopard <contact@jonleopard.com>
2023-07-08 11:54:41 -0700

    update plugin configs

--- nvim/.config/nvim/after/plugin/null-ls.lua
+++ nvim/.config/nvim/after/plugin/null-ls.lua
@@ -5,5 +5,6 @@ null_ls.setup({
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.diagnostics.vale,
     },
 })
--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all"
-    ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust", "go" },
+    ensure_installed = { "javascript", "typescript", "c", "lua", "rust", "go" },
     --ensure_installed = "all",
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
