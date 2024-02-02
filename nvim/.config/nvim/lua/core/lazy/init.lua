3c7e51d31051232b8bbb51efd2e0c624b3b6fbf2
Jon Leopard <contact@jonleopard.com>
2024-02-01 17:20:25 -0700

    fixed telescope trigger issue

--- nvim/.config/nvim/lua/core/lazy/init.lua
+++ nvim/.config/nvim/lua/core/lazy/init.lua
@@ -2,6 +2,15 @@ return {
     ------------------------------------------------------------------------------
     --Plugins that don't require their own file/setup
     ------------------------------------------------------------------------------
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build =
+        'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    },
     "mhinz/vim-startify",
     "editorconfig/editorconfig-vim",
     "nvim-tree/nvim-web-devicons",
--- nvim/.config/nvim/lua/core/lazy/telescope.lua
+++ nvim/.config/nvim/lua/core/lazy/telescope.lua
@@ -1,14 +1,23 @@
 return {
+
     "nvim-telescope/telescope.nvim",
-    {
-        "nvim-telescope/telescope-fzy-native.nvim",
-        build =
-        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
-    },
-    branch = '0.1.x',
+    tag = '0.1.5',
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup {
+            extensions = {
+                fzf = {
+                    fuzzy = true,                   -- false will only do exact matching
+                    override_generic_sorter = true, -- override the generic sorter
+                    override_file_sorter = true,    -- override the file sorter
+                    case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+                    -- the default case_mode is "smart_case"
+                }
+            }
+        }
+
+        require('telescope').load_extension('fzf')
+
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
@@ -26,7 +35,6 @@ return {
         vim.keymap.set('n', '<leader>ps', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
+    end,
 }
