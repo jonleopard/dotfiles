af0d90d7152cc867d1af45af0c1e2acf6affd949
Jon Leopard <contact@jonleopard.com>
2024-02-01 17:20:57 -0700

    less noise pls...

--- nvim/.config/nvim/lua/core/lazy_init.lua
+++ nvim/.config/nvim/lua/core/lazy_init.lua
@@ -11,4 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("core.lazy")
+require("lazy").setup({
+    spec = "core.lazy",
+    change_detection = { notify = false }
+})
