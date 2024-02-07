d4328c58c525f4a7556ffda782f755bbfca20fdb
Jon Leopard <contact@jonleopard.com>
2024-02-07 08:30:27 -0700

    gofmt imports

--- nvim/.config/nvim/lua/core/init.lua
+++ nvim/.config/nvim/lua/core/init.lua
@@ -13,6 +13,12 @@ function R(name)
     require("plenary.reload").reload_module(name)
 end
 
+vim.filetype.add({
+    extension = {
+        templ = 'templ',
+    }
+})
+
 autocmd('TextYankPost', {
     group = yank_group,
     pattern = '*',
@@ -47,6 +53,30 @@ autocmd('LspAttach', {
         vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
         vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
     end
+})
+
+-- Add imports on save
+autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({async = false})
+  end
 })
 
 vim.g.netrw_browse_split = 0
